3fda04640ce85bad615a77f4ce6ae4fd44fd0861
Jen Poole <contact.vandepool@gmail.com>
2018-02-19 22:35:13 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
+# create a table with ActiveRecord
+# To get access to the ActiveRecord methods, don't forget to inherit from ActiveRecord::Migration
 class CreateStudents < ActiveRecord::Migration
+  # define a method called change and use the ActiveRecord create_table method within that method to create the table
+  # The table should have a :name column with a type string
+  # run the migrations by running rake db:migrate in your terminal
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
